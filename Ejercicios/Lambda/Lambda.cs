2605716b859b75a48e7947587723e5b796734305
Tatiana Marcela Gallon Restrepo <tmgallon@cincod5.com>
2024-08-22 14:03:14 -0500

    Tarea

--- Ejercicios/Lambda/Lambda.cs
+++ Ejercicios/Lambda/Lambda.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Ejercicios.Lambda
+﻿namespace Ejercicios.Lambda
 {
     public class Lambda
     {
@@ -19,5 +13,59 @@ namespace Ejercicios.Lambda
                 Console.WriteLine(numero);
             }
         }
+
+        public void LambdaOrderList() {
+            string[] orderList = { "Ana", "Juan", "Tati" };
+
+            var nombresOrder = orderList.OrderBy(n => n.Length);
+
+            Console.WriteLine("Lista ordenada(Lambda Syntax):");
+            foreach (var nombre in nombresOrder)
+            {
+                Console.WriteLine(nombre);
+            }
+        }
+
+
+        public void LambdaConsultaNombre()
+        {
+
+            List<Person> personas = new List<Person>
+            {
+                new Person { Name = "Ana", Age = 23 },
+                new Person { Name = "Juan", Age = 30 },
+                new Person { Name= "Pedro",Age = 21 }
+            };
+
+            var nombresPersonasLambda = personas.Select(p => p.Name);
+
+            Console.WriteLine("Nombres de personas (Lambda Syntax):");
+            foreach (var nombre in nombresPersonasLambda)
+            {
+                Console.WriteLine(nombre);
+            }
+        }
+
+        public void LambdaConsultaNombreParametro(string name)
+        {
+
+            var people = new List<Person>
+            {
+                new Person { Name = "Ana", Age = 23 },
+                new Person { Name = "Juan", Age = 30 },
+                new Person { Name= "Pedro",Age = 21 }
+            };
+
+            Console.WriteLine("Existe nombre enviado:");
+            Console.WriteLine(people.Any(p => p.Name.Equals(name)));
+        }
+
+
+        //consulta nombres le pasamos un nombre para que obtenga el nombre, el metodo recibe un parametro y retorna si existe o no
+
+        //como manejar los nulos con lamda 
+
+        //usar consulta nombre con parametro string y retorne si existe o no el nombre 
+
     }
 }
--- Ejercicios/Linq/Linq.cs
+++ Ejercicios/Linq/Linq.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Ejercicios.Linq
 {
@@ -20,7 +21,63 @@ namespace Ejercicios.Linq
             {
                 Console.WriteLine(numero);
             }
+
         }
-        
+
+        public void QuerySintaxOrderList()
+        {
+            string[] nombres = { "Ana", "Pedro", "Maria" };
+
+            var orderName = from n in nombres
+                            orderby n.Length
+                            select n;
+
+
+            Console.WriteLine("Nombres ordenados");
+            foreach (var nombre in orderName) { 
+               Console.WriteLine($"El nombre es {nombre}");
+            
+            }
+        }
+
+        public void QuerySintaxConsultaNombres() {
+
+
+            List<Person> personas = new List<Person>
+            {
+                new Person { Name = "Ana", Age =23 },
+                new Person { Name = "Juan", Age=30 },
+                new Person { Name = "Pedro",Age=21 }
+            };
+
+            var personName = from p in personas
+                                select p.Name;
+
+            Console.WriteLine("Nombres de personas (Query Syntax):");
+            foreach (var name in personName)
+            {
+                Console.WriteLine(name);
+            }
+        }
+
+        public void QuerySintaxConsultaNombresParametro(string name)
+        {
+
+            var people = new List<Person>
+            {
+                new Person { Name = "Ana", Age = 23 },
+                new Person { Name = "Juan", Age = 30 },
+                new Person { Name= "Pedro",Age = 21 }
+            };
+
+            var personName = (from p in people
+                              where p.Name == name
+                              select p).Any();
+                             
+                          
+
+            Console.WriteLine("Existe nombre enviado:");
+            Console.WriteLine(personName);
+        }
     }
 }

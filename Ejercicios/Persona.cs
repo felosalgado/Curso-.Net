255e8d36cff8ee3f0bceda1e34cd77ec72d35ef7
MagelGD <miguel0504@live.com>
2024-08-22 15:55:58 -0500

    Se agrega cambios de las tareas

---
+++ Ejercicios/Persona.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicios
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public int edad {  get; set; }
+    }
+}
--- Ejercicios/Program.cs
+++ Ejercicios/Program.cs
@@ -3,6 +3,12 @@ using Ejercicios.Linq;
 
 Linq linq = new Linq();
 linq.QuerySintaxNumerosPares();
+linq.QuerySintaxOrdenaLista();
+linq.QuerySintaxConsultaNombre();
+linq.QuerySintaxConsultaNombreParametro("Pedro");
 
 Lambda lambda = new Lambda();
 lambda.LambdaNumerosPares();
+lambda.LambdaOrdenaLista();
+lambda.LambdaObtenerNombre();
+lambda.UtilizarNulls(null);

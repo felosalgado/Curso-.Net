57958fbe4ee0eaac437b8b283fdf2c4b8ecf79e5
felosalgado <felosalgado@gmail.com>
2024-08-30 12:43:55 -0500

    Test unit user controller

---
+++ Ejercicios/InyeccionDependencias/ConInterfaces/Interface/IMotor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicios.InyeccionDependencias.ConInterfaces.Interface
+{
+    public interface IMotor
+    {
+        void Start();
+    }
+}
---
+++ Ejercicios/InyeccionDependencias/ConInterfaces/MotorDiesel.cs
@@ -0,0 +1,15 @@
+﻿using Ejercicios.InyeccionDependencias.ConInterfaces.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicios.InyeccionDependencias.ConInterfaces
+{
+    public class MotorDiesel : IMotor
+    {
+        public void Start() => Console.WriteLine("Motor dicel encendido.");
+
+    }
+}

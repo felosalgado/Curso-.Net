df25dac3f2ebc6d2753b10fe3581392417570f4c
MagelGD <miguel0504@live.com>
2024-09-03 16:01:45 -0500

    Cambios Miguel

--- Ejercicios/Program.cs
+++ Ejercicios/Program.cs
@@ -19,3 +19,23 @@ lambda.LambdaNumerosPares();
 lambda.LambdaOrdenaLista();
 lambda.LambdaObtenerNombre();
 lambda.UtilizarNulls(null);
+//Console.WriteLine("Iniciando solicitud...");
+
+//Asincrona async = new Asincrona();
+// Llamada asíncrona a un método que realiza una solicitud HTTP
+//string content = await async.GetWebContentAsync("https://www.example.com");
+//Console.WriteLine("Contenido recibido:");
+//Console.WriteLine(content);
+
+// Crear un sensor de temperatura con un umbral de 30 grados
+TemperaturaSensor sensor = new TemperaturaSensor(30.0);
+// Crear un sistema de alerta
+TemperaturaAlerta alert = new TemperaturaAlerta();
+// Suscribirse al evento
+sensor.TemperaturaExcedida += alert.OnTemperatureExceeded;
+// Simular mediciones de temperatura
+double[] temperatures = { 25.0, 28.0, 31.5, 29.0, 33.0 };
+foreach (var temp in temperatures)
+{
+    sensor.VerificarTemperatura(temp);
+}

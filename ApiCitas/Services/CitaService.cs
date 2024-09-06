26c47cd9d5997204b539452ccd90b4efe753735e
YesicaDiosa <diosayesica7@gmail.com>
2024-09-06 14:59:05 -0500

    Parcial

---
+++ ApiCitas/Services/CitaService.cs
@@ -0,0 +1,38 @@
+﻿using ApiCitas.Data.Repositories;
+using ApiCitas.Models;
+
+namespace ApiCitas.Services
+{
+    public class CitaService : ICitaService
+    {
+        private readonly ICitaRepository _repository;
+        public CitaService(ICitaRepository repository)
+        {
+            _repository = repository;
+        }
+        public async Task<int> CreateCita(Cita cita)
+        {
+            return await _repository.Create(cita);
+        }
+
+        public async Task<int> DeleteCita(int id)
+        {
+            return await _repository.Delete(id);
+        }
+
+        public async Task<IEnumerable<Cita>> GetAllCitas()
+        {
+            return await _repository.GetAll();    
+        }
+
+        public async Task<Cita> GetCitaById(int id)
+        {
+            return await _repository.GetById(id);
+        }
+
+        public async Task<int> UpdateCita(Cita cita)
+        {
+            return await _repository.Update(cita);
+        }
+    }
+}

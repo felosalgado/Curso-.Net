cd72591b3c6f2a1ce6c90052bc4baffe9d556433
YesicaDiosa <diosayesica7@gmail.com>
2024-09-06 17:24:03 -0500

    Finalizado

---
+++ ApiCitas/Controllers/CitasController.cs
@@ -0,0 +1,65 @@
+﻿using ApiCitas.Models;
+using ApiCitas.Services;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ApiCitas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CitasController : ControllerBase
+    {
+        
+        private readonly ICitaService _citaService;
+        public CitasController(ICitaService service)
+        {
+            _citaService = service;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Cita>>> GetAllCitas()
+        {
+            var citas = await _citaService.GetAllCitas();
+            //IQueryable<cita> citasAsQueryable = citas.AsQueryable();
+            //IQueryable<cita> filter = citasAsQueryable.Where(e => e.citaName.Equals("Cristian"));
+
+            return Ok(citas);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Cita>> GetCitaById(int id)
+        {
+            var cita = await _citaService.GetCitaById(id);
+            if (cita == null)
+            {
+                return NotFound();
+            }
+            return Ok(cita);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateCita(Cita cita)
+        {
+            await _citaService.CreateCita(cita);
+            return CreatedAtAction(nameof(GetCitaById), new { id = cita.CitaID }, cita);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateCita(int id, Cita cita)
+        {
+            if (id != cita.CitaID)
+            {
+                return BadRequest();
+            }
+            await _citaService.UpdateCita(cita);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteCita(int id)
+        {
+            await _citaService.DeleteCita(id);
+            return NoContent();
+        }
+    }
+}

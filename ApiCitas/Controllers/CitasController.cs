4f8f91762da0a06eb54c7802e91989f2356fcfd4
Juan David Suarez Reyes <desarrolladorti14@cincod5.com>
2024-09-06 17:47:24 -0500

    Commit Documentacion

--- ApiCitas/Controllers/CitasController.cs
+++ ApiCitas/Controllers/CitasController.cs
@@ -16,6 +16,11 @@ namespace ApiCitas.Controllers
         {
             _citaService = citaService;
         }
+
+        /// <summary>
+        /// Obtiene citas de la entidad
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Citas>>> GetAllCitas()
         {
@@ -25,6 +30,11 @@ namespace ApiCitas.Controllers
 
             return Ok(citas);
         }
+        /// <summary>
+        /// Consulta Citas por ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
 
         [HttpGet("{id}")]
         public async Task<ActionResult<User>> GetCitaById(int id)
@@ -38,14 +48,23 @@ namespace ApiCitas.Controllers
             }
             return Ok(citas);
         }
-
+        /// <summary>
+        /// Crea las citas
+        /// </summary>
+        /// <param name="cita"></param>
+        /// <returns></returns>
         [HttpPost]
         public async Task<ActionResult> CreateCita(Citas cita)
         {
             await _citaService.CreateCita(cita);
             return CreatedAtAction(nameof(GetCitaById), new { id = cita.CitaID }, cita);
         }
-
+        /// <summary>
+        /// actualiza datos de la cita por ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="cita"></param>
+        /// <returns></returns>
         [HttpPut("{id}")]
         public async Task<ActionResult> UpdateCita(int id, Citas cita)
         {
@@ -56,7 +75,11 @@ namespace ApiCitas.Controllers
             await _citaService.UpdateCita(cita);
             return NoContent();
         }
-
+        /// <summary>
+        /// Borra citas por ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpDelete("{id}")]
         public async Task<ActionResult> DeleteCita(int id)
         {

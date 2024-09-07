2aa80a531fca5b6f40cc8f1a9970248614ff6c91
Carlos Mario Rios Yepes <carlos.mario10@hotmail.es>
2024-09-06 22:57:01 -0500

    terminar pruebas unitrias

---
+++ TestApiCitas/Mocks/MockDataCitas.cs
@@ -0,0 +1,26 @@
+﻿using ApiCitas.Domain.Models;
+
+namespace TestApiCitas.Mocks
+{
+    public static class MockDataCitas
+    {
+        public static List<Cita> GetMockCitas()
+        {
+            return new List<Cita>
+            {
+                new Cita { Descripcion = "Descripcion 1", Estado = "Estado 1", Lugar = "Lugar 1" },
+                new Cita { Descripcion = "Descripcion 2", Estado = "Estado 2", Lugar = "Lugar 2" }
+            };
+        }
+
+        public static Cita GetMockCita()
+        {
+            return new Cita
+            {
+                Descripcion = "Descripcion 1",
+                Estado = "Estado 1",
+                Lugar = "Lugar 1"
+            };
+        }
+    }
+}
---
+++ TestApiCitas/Test/TestCitas.cs
@@ -0,0 +1,52 @@
+﻿using ApiCitas.Application.Services;
+using ApiCitas.Domain.Contracts;
+using ApiCitas.Domain.Models;
+using Moq;
+using System.Net;
+using TestApiCitas.Mocks;
+
+namespace TestApiCitas.Test
+{
+    [TestClass]
+    public class TestCitas
+    {
+        [TestMethod]
+        public void GetCitas_Ok()
+        {
+            //Arrange
+            var mockCitas = MockDataCitas.GetMockCitas();
+            Mock<ICitaRepository> citaRepository = new();
+            CitaServices citaServicio = new(citaRepository.Object);
+
+            citaRepository.Setup(a => a.GetAll()).Returns(Task.FromResult<IEnumerable<Cita>>(mockCitas));
+
+            //Act
+            var citas = citaServicio.GetAllCitas();
+
+            //Assert
+            Assert.AreEqual("Lugar 1", citas.Result.FirstOrDefault().Lugar);
+            Assert.IsNotNull(citas.Result);
+            citaRepository.Verify(a => a.GetAll(), Times.Once);
+        }
+
+        [TestMethod]
+        public void GetCita_Ok()
+        {
+            //Arrange
+            var mockCita = MockDataCitas.GetMockCita();
+            Mock<ICitaRepository> citaRepository = new();
+            CitaServices citaServicio = new(citaRepository.Object);
+
+            citaRepository.Setup(a => a.GetById(1)).Returns(Task.FromResult<Cita>(mockCita));
+
+            //Act
+            var cita = citaServicio.GetCitaById(1);
+
+            //Assert
+            Assert.AreEqual("Estado 1", cita.Result.Estado);
+            Assert.AreEqual("Descripcion 1", cita.Result.Descripcion);
+            Assert.IsNotNull(cita.Result);
+            citaRepository.Verify(a => a.GetById(1), Times.Once);
+        }
+    }
+}
